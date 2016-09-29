b8cfb156035f131b24400266f51fcda60e022051
Eric Velten de Melo <eric.velten@eldorado.org.br>
2016-09-29 19:25:34 -0300

    work in progress

--- app/MainGTK.hs
+++ app/MainGTK.hs
@@ -17,13 +17,25 @@ import Translator
 --updateBuffers :: (TextBufferClass self) => self -> SentenceBlock
 
 changeCursor :: (Zipper a -> Zipper a) -> Zipper a -> IO (Zipper a, a)
-changeCursor f zip = return (new, cursor new) 
+changeCursor f zip = return (new, cursor new)
     where new = f zip
 
+updateSentenceBlock :: String -> SentenceBlock -> SentenceBlock
+updateSentenceBlock text (SentenceBlock n orig _) = SentenceBlock n orig text
+
+textViewGetValue :: TextViewClass self => self -> IO String
+textViewGetValue tv = do
+    buf <- textViewGetBuffer tv
+    start <- textBufferGetStartIter buf
+    end <- textBufferGetEndIter buf
+    value <- textBufferGetText buf start end True
+    return value
 
 main = do
     [file,page] <- getArgs
+    --load page sentences
     sentences <- fmap (makeZipper . getSentences) $ openPdfFile file (read page)
+    --create zipper
     zipper <- newMVar sentences
     initGUI
 
@@ -40,14 +52,17 @@ main = do
     nextButton <- builderGetObject builder castToButton "nextbutton"
     on nextButton buttonActivated $ do
         block <- modifyMVar zipper (changeCursor right)
-        textBufferSetText buffer1 $ T.unpack $ originalText block 
-        textBufferSetText buffer2 $ T.unpack $ translatedText block 
+        textBufferSetText buffer1 $ T.unpack $ originalText block
+        textBufferSetText buffer2 $ T.unpack $ translatedText block
 
     prevButton <- builderGetObject builder castToButton "previousbutton"
     on prevButton buttonActivated $ do
+        text2 <- textBufferGetValue buffer2
+        --let currentBlock = updateSentenceBlock 
+        block <- modifyMVar insert text zipper
         block <- modifyMVar zipper (changeCursor left)
-        textBufferSetText buffer1 $ T.unpack $ originalText block 
-        textBufferSetText buffer2 $ T.unpack $ translatedText block 
+        textBufferSetText buffer1 $ T.unpack $ originalText block
+        textBufferSetText buffer2 $ T.unpack $ translatedText block
 
     current <- withMVar zipper (return . cursor)
     textBufferSetText buffer1 $ T.unpack $ originalText current
